f4861344632430c02b27937957262b9694c650d0
ducndbk62 <39408329+ducndbk62@users.noreply.github.com>
2019-12-07 13:57:01 +0700

    Add files via upload

---
+++ process_enwiki_botag.py
@@ -0,0 +1,30 @@
+import re
+
+#mo file txt
+file = open("enwiki/enwiki-20191101-3.txt", "r", errors='ignore')
+
+with open('bodoc3.txt', 'w') as f: #file ket qua
+    article_txt = file.read(80000) #doc file den khi het file
+    while article_txt != '':
+        # bo tab doc
+        article_txt = re.sub(r"<doc.*>", "", article_txt)
+        article_txt = re.sub(r"<\/doc>", "", article_txt)
+
+        #bo cac dau cau
+        article_txt = re.sub(
+            r"\"|\#|\$|\%|\&amp;|\(|\)|\*|\+|\,|\-|\–|\/|\:|\;|\|\@|\[|\\|\]|\^|\_|\`|\{|\||\}|\~|\<|\>|\=", " ",
+            article_txt)
+        article_txt = re.sub(r"\n", " ", article_txt)
+        article_txt = re.sub(r"\'\'", " ", article_txt)
+
+        #bo cac chu so
+        article_txt = re.sub(r"\d+", "", article_txt)
+
+        #bo dau cach thua
+        article_txt = re.sub(r"  ", " ", article_txt)
+
+        f.write(article_txt) #ghi file
+        article_txt = file.read(80000)
+    f.close()
+
+print("ok")
